f60e6932409c6b4a110210c8a1342dde952867fa
aredkin-u <63556704+aredkin-u@users.noreply.github.com>
2024-05-01 13:58:57 +0300

    new code and new files

--- module_0/main.py
+++ module_0/main.py
@@ -1,42 +1,27 @@
 import numpy as np
 
-def game_core_v3(number):
-    '''реализуем алгоритм бинарного поиска'''
-    count = 1
-    start_int = 1
-    end_int = 100
-    # Для начала сделаем проверку на конец интервала
-    predict = 100
-    while number != predict and count < 100:
+def game_core_v3(number: int = 1) -> int:
+    """'''реализуем алгоритм бинарного поиска'''
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
 
-        # Если это не конец интервала, тогда начинаем бинарный поиск
-        if count == 1:
-            predict = 50
-
+    Returns:
+        int: Число попыток
+    """
+    start_element = 1 
+    end_element = 100
+    count = 0
+    found_num = 0
+    
+    # Условие выхода из цикла - нахождение числа или превышение количества попыток
+    while number != found_num and count < 100:
         count+=1
-        if predict > number :
-            end_int = predict
-        elif predict < number:
-            start_int = predict
-        predict = (start_int+end_int)//2
-
-    return(count) # выход из цикла, если угадали
-
-
-def score_game(game_core):
-    '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
-    count_ls = []
-    np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
-    random_array = np.random.randint(1,101, size=(1000))
-    for number in random_array:
-        count_ls.append(game_core(number))
-    score = int(np.mean(count_ls))
-    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
-    return(score)
-
-
-# Проверяем
-score_game(game_core_v3)
-#import pandas as pd
-#pd.test()
-#football = pd.read_csv('D:\Projects\skillfactory_rds\module_0\data_sf.csv')
+        middle_element = start_element + (end_element - start_element) // 2
+        if middle_element == number:
+            found_num = middle_element
+        elif middle_element < number:
+            start_element = middle_element + 1
+        else:
+            end_element = middle_element - 1
+   
+    return count # Возвращаем количество попыток
